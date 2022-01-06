650cf684a64445cdd91b9ea80335ae121d7a48a7
Evgeniy <zhel186@mail.ru>
2022-01-06 17:23:45 +0300

    added bean cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean.equals(bean2));
+        System.out.println(bean3.equals(bean4));
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String  breed;
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
