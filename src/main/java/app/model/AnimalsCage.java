cadd46459acded80c539643c064f26f78f772c1c
Evgeniy <zhel186@mail.ru>
2022-01-07 20:09:20 +0300

    added Cat

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,23 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    @Autowired
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
